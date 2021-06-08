f5e8a43aa686ba925d6a15507928a534c7c80376
Vitor Akira <vitorakira1@gmail.com>
2021-06-08 18:22:44 -0300

    add header

---
+++ src/pages/estoque/index.tsx
@@ -0,0 +1,15 @@
+import Head from "next/head";
+
+export default function Stock() {
+    return (
+        <>
+            <Head>
+                <title>Estoque | Artha</title>
+            </Head>
+
+            <main>
+                <h1>Estoque</h1>
+            </main>
+        </>
+    );
+}
---
+++ src/pages/fornecedor/index.tsx
@@ -0,0 +1,15 @@
+import Head from "next/head";
+
+export default function Stock() {
+    return (
+        <>
+            <Head>
+                <title>Fornecedor | Artha</title>
+            </Head>
+
+            <main>
+                <h1>Fornecedor</h1>
+            </main>
+        </>
+    );
+}
