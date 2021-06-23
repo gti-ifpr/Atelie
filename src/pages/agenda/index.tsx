2215f57a470022f5f40c792ab8af697e3c03fccc
daserra <and_ctba@hotmail.com>
2021-06-23 21:16:16 +0200

    Refactor compromissos table

--- src/pages/agenda/index.tsx
+++ src/pages/agenda/index.tsx
@@ -1,133 +1,131 @@
 import { GetServerSideProps } from "next";
 import Head from "next/head";
-import { useState } from "react";
+import { FunctionComponent, useState } from "react";
 import { ScheduleHeader } from "../../components/Schedule/ScheduleHeader";
 import { api } from "../../services/api";
-import { getCurrentDateHourInString } from "../../utils/getCurrentDateInString";
 
 type Compromisso = {
-    id: number;
-    horarioInicio: string;
-    horarioTermino: string;
-    dataAgendadaString: string;
-    dataAgendadaDayOfTheWeek: number;
-    selectedClient: number;
-    tipo: string;
-    status: string;
+  id: number;
+  horarioInicio: string;
+  horarioTermino: string;
+  dataAgendadaString: string;
+  dataAgendadaDayOfTheWeek: number;
+  selectedClient: number;
+  tipo: string;
+  status: string;
 };
 
 type CompromissoProps = {
-    schedule: Compromisso[];
+  compromissos: Compromisso[];
 };
 
-export default function Agenda({ schedule }: CompromissoProps) {
-    const [filtrarPorDia, setFiltrarPorDia] = useState("hoje");
+type FilterType = "hoje" | "semana" | "semFiltro";
 
+const CompromissoRow: FunctionComponent<{ compromisso: Compromisso }> = ({
+  compromisso,
+}) => {
+  return (
+    <>
+      <tr key={compromisso.id}>
+        <td>{compromisso.selectedClient}</td>
+        <td>
+          {compromisso.horarioInicio} - {compromisso.horarioTermino}
+        </td>
+        <td>{compromisso.tipo}</td>
+        <td>{compromisso.status}</td>
+        <td></td>
+      </tr>
+    </>
+  );
+};
 
-    return (
-        <>
-            <Head>
-                <title>Agenda | Artha</title>
-            </Head>
-
-            <main>
-                <ScheduleHeader />
-
-                <button onClick={() => setFiltrarPorDia("hoje")}>Hoje</button>
-                <button onClick={() => setFiltrarPorDia("semana")}>Semana</button>
-                <button onClick={() => setFiltrarPorDia("todos")}>Todos</button>
-                <table>
-                    <thead>
-                        <tr>
-                            <th>Cliente</th>
-                            <th>Ínicio-Término</th>
-                            <th>Tipo</th>
-                            <th>Status</th>
-                            <th></th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {filtrarPorDia === "hoje" &&
-
-                          const compromissosDoDia = schedule.filter(
-                            (compromisso) => compromisso.dataAgendadaString === getCurrentDateHourInString(new Date())
-                        );
-                    
-                            compromissosDoDia.map((schedule) => (
-                        <tr key={schedule.id}>
-                            <td>{schedule.selectedClient}</td>
-                            <td>
-                                {schedule.horarioInicio} - {schedule.horarioTermino}
-                            </td>
-                            <td>{schedule.tipo}</td>
-                            <td>{schedule.status}</td>
-                            <td>{schedule.dataAgendadaString}</td>
-                        </tr>
-                            ))}
-
-                        {filtrarPorDia === "todos" &&
-                            schedule.map((schedule) => (
-                                <tr key={schedule.id}>
-                                    <td>{schedule.selectedClient}</td>
-                                    <td>
-                                        {schedule.horarioInicio} - {schedule.horarioTermino}
-                                    </td>
-                                    <td>{schedule.tipo}</td>
-                                    <td>{schedule.status}</td>
-                                    <td></td>
-                                </tr>
-                            ))}
-
-                        {filtrarPorDia === "todos" &&
-                            schedule.map((schedule) => (
-                                <tr key={schedule.id}>
-                                    <td>{schedule.selectedClient}</td>
-                                    <td>
-                                        {schedule.horarioInicio} - {schedule.horarioTermino}
-                                    </td>
-                                    <td>{schedule.tipo}</td>
-                                    <td>{schedule.status}</td>
-                                    <td></td>
-                                </tr>
-                            ))}
-
-                        {/* {filtrarPorDia === "semana" &&
-                            <filtrarPorSemana />
-                        } */}
-                    </tbody>
-                </table>
-            </main>
-        </>
-    );
+function filterCompromissoByType(
+  filterType: FilterType,
+  compromissos: Compromisso[]
+) {
+  switch (filterType) {
+    case "hoje":
+      return compromissos.filter(
+        (compromisso) =>
+          compromisso.dataAgendadaString ===
+          "getCurrentDateHourInString(new Date())" //TODO Filtrar por dia
+      );
+      break;
+    case "semana":
+      return compromissos.filter(
+        (compromisso) =>
+          compromisso.dataAgendadaString ===
+          "getCurrentDateHourInString(new Date())" //TODO Filtrar por semana
+      );
+    default:
+      return compromissos;
+  }
 }
 
+export default function Agenda({ compromissos }: CompromissoProps) {
+  const [compromissoFilter, setCompromissoFilter] =
+    useState<FilterType>("hoje");
 
+  compromissos = filterCompromissoByType(compromissoFilter, compromissos);
+
+  return (
+    <>
+      <Head>
+        <title>Agenda | Artha</title>
+      </Head>
+
+      <main>
+        <ScheduleHeader />
+
+        <button onClick={() => setCompromissoFilter("hoje")}>Hoje</button>
+        <button onClick={() => setCompromissoFilter("semana")}>Semana</button>
+        <button onClick={() => setCompromissoFilter("semFiltro")}>Todos</button>
+        <table>
+          <thead>
+            <tr>
+              <th>Cliente</th>
+              <th>Ínicio-Término</th>
+              <th>Tipo</th>
+              <th>Status</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            {compromissos.map((compromisso) => (
+              <CompromissoRow compromisso={compromisso} />
+            ))}
+          </tbody>
+        </table>
+      </main>
+    </>
+  );
+}
 
 export const getServerSideProps: GetServerSideProps = async () => {
-    const { data } = await api.get("/schedule", {
-        params: {
-            _limit: 7,
-            _sort: "data",
-            _order: "incr",
-        },
-    });
+  const { data } = await api.get("/schedule", {
+    params: {
+      _limit: 7,
+      _sort: "data",
+      _order: "incr",
+    },
+  });
 
-    const compromissos = data.map((compromisso) => {
-        return {
-            id: compromisso.id,
-            horarioInicio: compromisso.horario_inicio,
-            horarioTermino: compromisso.horario_termino,
-            dataAgendadaString: compromisso.data_agendada,
-            dataAgendadaDayOfTheWeek: new Date(compromisso.data_agendada).getDay(),
-            selectedClient: compromisso.cliente_selecionado,
-            tipo: compromisso.tipo_compromisso,
-            status: compromisso.compromisso_status,
-        };
-    });
-
+  const compromissos = data.map((compromisso) => {
     return {
-        props: {
-            schedule: compromissos,
-        },
+      id: compromisso.id,
+      horarioInicio: compromisso.horario_inicio,
+      horarioTermino: compromisso.horario_termino,
+      dataAgendadaString: compromisso.data_agendada,
+      dataAgendadaDayOfTheWeek: new Date(compromisso.data_agendada).getDay(),
+      selectedClient: compromisso.cliente_selecionado,
+      tipo: compromisso.tipo_compromisso,
+      status: compromisso.compromisso_status,
     };
+  });
+
+  return {
+    props: {
+      schedule: compromissos,
+    },
+  };
 };
