ca413f65c04516878054279a522151aafbde88cb
Vitor Akira <vitorakira1@gmail.com>
2021-08-05 14:48:43 -0300

    add technical file page

---
+++ src/pages/fichaTecnica/index.tsx
@@ -0,0 +1,14 @@
+import Head from "next/head";
+
+export default function fichaTecnica() {
+    return (
+        <>
+            <Head>
+                <title>Clientes | Artha</title>
+            </Head>
+            <main>
+                <h1>Oi</h1>
+            </main>
+        </>
+    )
+}
