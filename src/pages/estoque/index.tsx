a9308ab4295441efa7422dc4b981a0298799fe3b
Vitor Akira <vitorakira1@gmail.com>
2021-09-10 14:22:31 -0300

    centralize buttons on stock

--- src/pages/estoque/index.tsx
+++ src/pages/estoque/index.tsx
@@ -3,14 +3,17 @@ import { Button } from "../../components/Button/button";
 import { ClothsInStock } from "../../components/ClothsInStock/ClothsInStock";
 import { FabricsInStock } from "../../components/FabricsInStock/FabricsInStock";
 
+import styles from './styles.module.scss';
+
 export default function Stock() {
     const [isClothStockActive, setIsClothStockActive] = useState(true);
 
     return (
         <>
-            <Button isActive={isClothStockActive} onClick={() => setIsClothStockActive(true)}>Roupas</Button>
-            <Button isActive={!isClothStockActive} onClick={() => setIsClothStockActive(false)}>Tecidos</Button>
-
+            <div className={styles.stockButtonContainer}>
+                <Button isActive={isClothStockActive} onClick={() => setIsClothStockActive(true)}>Roupas</Button>
+                <Button isActive={!isClothStockActive} onClick={() => setIsClothStockActive(false)}>Tecidos</Button>
+            </div>
 
             {isClothStockActive ?
                 <ClothsInStock />
